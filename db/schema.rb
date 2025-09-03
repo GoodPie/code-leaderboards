11d8ee4fa8042aa0ea0f04d428a5c80886bf670e
Brandyn Britton <brandynbb96@gmail.com>
2025-09-03 12:44:11 +0800

    fix(actions): updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -24,10 +24,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_03_041910) do
   create_schema "vault"
 
   # These are extensions that must be enabled in order to support this database
-  enable_extension "extensions.pg_net"
-  enable_extension "extensions.pg_stat_statements"
-  enable_extension "extensions.pgcrypto"
-  enable_extension "extensions.uuid-ossp"
   enable_extension "graphql.pg_graphql"
   enable_extension "pg_catalog.plpgsql"
   enable_extension "vault.supabase_vault"
